c5f337450f18fb5e060dea60784974f27fd092bd
PhilippMeissner <pm@zeit.io>
2016-02-18 18:23:29 +0100

    addewd fadeout for songsearch

--- resources/js/views/SongSearchView.js
+++ resources/js/views/SongSearchView.js
@@ -1,31 +1,35 @@
 /*
 * The MIT License (MIT)
-* 
+*
 * Copyright (c) 2014 Richard Backhouse
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 * DEALINGS IN THE SOFTWARE.
 */
 define([
-		'jquery', 
+		'jquery',
 		'backbone',
-		'underscore', 
+		'underscore',
 		'./BaseView',
 		'../models/SongSearchList',
-		'text!templates/SongSearch.html'], 
+		'text!templates/SongSearch.html'],
 function($, Backbone, _, BaseView, SongSearchList, template){
 	var View = BaseView.extend({
 		events: function() {
 		    return _.extend({}, BaseView.prototype.events, {
-		    });	
+					"click .addSong": function(evt) {
+						var clickedItem = $(evt.target).closest("li");
+						$(clickedItem).fadeTo( "slow", 0.5 );
+					}
+		    });
 		},
 		initialize: function(options) {
 			this.songSearchList = new SongSearchList({});
@@ -35,7 +39,7 @@ function($, Backbone, _, BaseView, SongSearchList, template){
 			this.constructor.__super__.initialize.apply(this, [options]);
 			this.template = _.template( template) ( {total: 0} );
 			$.mobile.document.one("filterablecreate", "#songList", function() {
-				$("#songList").on( "filterablebeforefilter", function(e, data) { 
+				$("#songList").on( "filterablebeforefilter", function(e, data) {
 					e.preventDefault();
 		            var $input = $( data.input );
 		            var value = $input.val();
@@ -64,7 +68,7 @@ function($, Backbone, _, BaseView, SongSearchList, template){
 					$.mobile.loading("hide");
 					$("#songList li").remove();
 					collection.each(function(song) {
-						$("#songList").append("<li data-icon=\"plus\"><a href='#playlist/song/"+song.get("b64file")+"'><p style=\"white-space:normal\">"+song.get("title")+" : " + song.get("artist") + " : "+song.get("album")+"</p></a></li>");
+						$("#songList").append("<li class='addSong' data-icon=\"plus\"><a href='#playlist/song/"+song.get("b64file")+"'><p style=\"white-space:normal\">"+song.get("title")+" : " + song.get("artist") + " : "+song.get("album")+"</p></a></li>");
 					});
 					$("#songList").listview('refresh');
 					$("#total").text(collection.length);
@@ -76,6 +80,6 @@ function($, Backbone, _, BaseView, SongSearchList, template){
 			});
 		}
 	});
-	
+
 	return View;
 });
